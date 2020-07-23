0bf45ace33586fc40396578ef13a280ddced3693
Ajtheult <asaphfranksboi@gmail.com>
2020-07-23 13:20:50 +0300

    Refactory project directory. Update Atlas to allow public access. Update concurrently scripts.

---
+++ .history/src/layout/pages/login/Login_20200723115105.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../../../App.css";
+import background5 from "../imagesofinstruments/background5.jpg";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="bg-color">
+        <img src={background5} className="bg" alt="bg" />
+      </div>
+      <div className="my_login-box">
+        <h1 className="white">Login</h1>
+        <div className="my_textbox">
+          <input type="text" placeholder="Fullname" name="true" required />
+
+          <input type="password" placeholder="Password" name="true" required />
+        </div>
+
+        <input className="my_btn" type="button" name="" value="Sign in" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
